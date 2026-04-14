f65a460ac3671e4cd3c79aee621b66c52284a8c1
DuBo Lee <dubolee46697@gmail.com>
2026-04-14 16:16:52 +0800

    Add some extended features, partly fixed some vulnerbilities.

---
+++ sqrtalterv13.py
@@ -0,0 +1,136 @@
+# This is a program to test whether the squart algorithm is True or not, written in python
+# Version 1.3, supports fraction, but the output digit cannot be defined by user, meanwhile, we found a vurnable and fixed it partly.
+
+a = int(input("Input the integer part:"))
+arec = int(input("Input the fraction part:"))
+b = str(a)
+arecstr = str(arec)
+
+def fir_num(b):
+    if len(b) % 2 == 1:
+        return b[0]
+    else:
+        return str(b[0]) + str(b[1])
+
+def div_num(b):
+    if len(b) % 2 == 1:
+        return (len(b) + 1) / 2
+    else:
+        return len(b) / 2
+
+def div_arec(arec):
+    if len(arecstr) % 2 == 1:
+        return (len(arecstr) + 1) / 2
+    else:
+        return len(arecstr) / 2
+
+ansfr_len = div_arec(arec)
+
+def firblo(x):
+    i = 1
+    while i < 10:
+        k = i*i
+        j = (i + 1)*(i + 1)
+        if k <= int(x) and j > int(x):
+            return i
+        else:
+            i = i + 1
+
+def anspro(b):
+    ans = ''
+    ans_len = div_num(b)
+    fir_var = fir_num(b)
+    fir_dig = str(firblo(fir_var))
+    ans = ans + fir_dig
+    fir_rem = int(fir_num(b)) - int(fir_dig)*int(fir_dig)
+    rem = fir_rem
+    anspp = int(ans)
+    for i in range(2, int(ans_len + 1)):
+        if len(b) % 2 == 1:
+            rec_var = str(b[2*i - 3]) + str(b[2*i - 2])
+        else:
+            rec_var = str(b[2*i - 2]) + str(b[2*i - 1])
+        rem = str(rem) + rec_var
+        if int(str(2*anspp) + str(1)) > int(rem):
+            ans = ans + str(0)
+            rem = rem
+        else:
+            for j in range(1, 10):
+                ans_try = int(str(2*anspp) + str(j))*j
+                ans_trym = int(str(2*anspp) + str(j + 1))*(j + 1)
+                if ans_try <= int(rem) and ans_trym > int(rem):
+                    ans = ans + str(j)
+                    rem = str(int(rem) - ans_try)
+                    break
+                else:
+                    j = j + 1
+        i = i + 1
+        anspp = int(ans)
+    return ans
+
+ansint = int(anspro(b))
+
+def ansarec(arecstr):
+    ansstr = str(ansint)
+    rem = str(int(b) - (ansint*ansint))
+    if len(arecstr) % 2 == 0:
+        for i in range(1, int(ansfr_len) + 1):
+            rec_var = str(arecstr[2*i - 2]) + str(arecstr[2*i - 1])
+            rem = rem + rec_var
+            if int(str(2*int(ansstr)) + str(1)) > int(rem):
+                ansstr = ansstr + str(0)
+                rem = rem
+            elif int(str(2*int(ansstr)) + str(9))*9 < int(rem):
+                rem = int(rem) - int(str(2*int(ansstr)) + str(9))*9
+                ansstr = ansstr + str(9)
+            else:
+                for j in range(1, 10):
+                    ans_try = int(str(2*int(ansstr)) + str(j))*j
+                    ans_trym = int(str(2*int(ansstr)) + str(j + 1))*(j + 1)
+                    if ans_try <= int(rem) and ans_trym > int(rem):
+                        ansstr = ansstr + str(j)
+                        rem = str(int(rem) - ans_try)
+                        break
+                    else:
+                        j = j + 1
+    else:
+        for i in range(1, int(ansfr_len)):
+            rec_var = str(arecstr[2*i - 2]) + str(arecstr[2*i - 1])
+            rem = rem + rec_var
+            if int(str(2*int(ansstr)) + str(1)) > int(rem):
+                ansstr = ansstr + str(0)
+                rem = rem
+            elif int(str(2*int(ansstr)) + str(9))*9 < int(rem):
+                rem = int(rem) - int(str(2*int(ansstr)) + str(9))*9
+                ansstr = ansstr + str(9)
+            else:
+                for j in range(1, 10):
+                    ans_try = int(str(2*int(ansstr)) + str(j))*j
+                    ans_trym = int(str(2*int(ansstr)) + str(j + 1))*(j + 1)
+                    if ans_try <= int(rem) and ans_trym > int(rem):
+                        ansstr = ansstr + str(j)
+                        rem = str(int(rem) - ans_try)
+                        break
+                    else:
+                        j = j + 1
+        rem = str(rem) + arecstr[len(arecstr) - 1] + '0'
+        if int(str(2*int(ansstr)) + str(1)) > int(rem):
+            ansstr = ansstr + str(0)
+            rem = rem
+        elif int(str(2*int(ansstr)) + str(9))*9 < int(rem):
+            rem = int(rem) - int(str(2*int(ansstr)) + str(9))*9
+            ansstr = ansstr + str(9)
+        else:
+            for j in range(1, 10):
+                ans_try = int(str(2*int(ansstr)) + str(j))*j
+                ans_trym = int(str(2*int(ansstr)) + str(j + 1))*(j + 1)
+                if ans_try <= int(rem) and ans_trym > int(rem):
+                    ansstr = ansstr + str(j)
+                    rem = str(int(rem) - ans_try)
+                    break
+                else:
+                    j = j + 1
+    return ansstr
+
+
+ansarec(arecstr)
